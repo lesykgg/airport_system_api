fe2180bbd6e5522afbd64accaa26e7a0e9a09437
Oleksii Hashparych <lesykgg@gmail.com>
2021-11-01 23:59:28 +0200

    Expose authorization header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localho.st', 'http://localhost:3000', 'http://localhost:5000', 'https://localhost:3000'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put], credentials: true, expose: %w(Authorization), max_age: 600
   end
 end
